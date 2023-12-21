7128548129ae2d6fcb08b9a8c3c945afa72c50d5
robuGdada <paudelronish@gmail.com>
2023-12-21 14:53:54 +0545

    new queryClient set

--- App.tsx
+++ App.tsx
@@ -2,13 +2,24 @@ import React from "react";
 import Footer from "./src/components/footer";
 import Main from "./src/components/main";
 import Header from "./src/components/header";
+import { View, StyleSheet } from "react-native";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default function App() {
+  const queryClient = new QueryClient();
   return (
-    <>
-      <Header />
-      <Main />
-      <Footer />
-    </>
+    <View style={styles.container}>
+      <QueryClientProvider client={queryClient}>
+        <Header />
+        <Main />
+        {/* <Footer /> */}
+      </QueryClientProvider>
+    </View>
   );
 }
