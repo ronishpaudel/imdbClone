9782c305cf60ff56a6b31497a88c3e1b84256f38
robuGdada <paudelronish@gmail.com>
2023-12-21 15:13:23 +0545

    env configuration

---
+++ env.d.ts
@@ -0,0 +1,6 @@
+declare module "@env" {
+  export const API_URL: string;
+  export const APP_MAJOR_VERSION: string;
+  export const APP_MINOR_VERSION: string;
+  export const APP_PATCH_VERSION: string;
+}
