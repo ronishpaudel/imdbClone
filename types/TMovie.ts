d1530b1d7bb617b1d3787f5d0f0c9b0b0303488d
robuGdada <paudelronish@gmail.com>
2023-12-22 19:39:13 +0545

    type update

--- types/TMovie.ts
+++ types/TMovie.ts
@@ -1,16 +1,16 @@
 export type TMovie = {
-  adult: boolean;
-  backdrop_path: string;
-  genre_ids: number[];
+  adult?: boolean;
+  backdrop_path?: string;
+  genre_ids?: number[];
   id: number;
-  original_language: string;
-  original_title: string;
+  original_language?: string;
+  original_title?: string;
   overview: string;
-  popularity: number;
+  popularity?: number;
   poster_path: string;
-  release_date: string;
+  release_date?: string;
   title: string;
-  video: boolean;
-  vote_average: number;
-  vote_count: number;
+  video?: boolean;
+  vote_average?: number;
+  vote_count?: number;
 };
