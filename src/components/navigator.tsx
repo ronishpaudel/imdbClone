79e3f61b1ae7bcf268f7ac322a0247e607af3b9a
robuGdada <paudelronish@gmail.com>
2023-12-22 19:41:06 +0545

    navigator bar add

---
+++ src/components/navigator.tsx
@@ -0,0 +1,63 @@
+import * as React from "react";
+import {
+  CompositeScreenProps,
+  NavigationContainer,
+  NavigatorScreenParams,
+} from "@react-navigation/native";
+import {
+  NativeStackScreenProps,
+  createNativeStackNavigator,
+} from "@react-navigation/native-stack";
+import Header from "./header";
+import { TMovie } from "../../types/TMovie";
+import { Details } from "./details";
+import { BottomTabScreenProps } from "@react-navigation/bottom-tabs";
+import { MyTabs } from "./tabBar";
+
+export type RootStackParamList = {
+  Main: NavigatorScreenParams<HomeTabParamList>;
+  Details: TMovie;
+  MyTabs: any;
+};
+
+export type HomeTabParamList = {
+  Popular: undefined;
+  Latest: undefined;
+};
+
+export type HomeTabScreenProps<T extends keyof HomeTabParamList> =
+  CompositeScreenProps<
+    BottomTabScreenProps<HomeTabParamList, T>,
+    RootStackScreenProps<keyof RootStackParamList>
+  >;
+
+export type RootStackScreenProps<T extends keyof RootStackParamList> =
+  NativeStackScreenProps<RootStackParamList, T>;
+
+declare global {
+  namespace ReactNavigation {
+    interface RootParamList extends RootStackParamList {}
+  }
+}
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+function Navigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="MyTabs"
+          options={{ header: () => <Header /> }}
+          component={MyTabs}
+        />
+        <Stack.Screen
+          name="Details"
+          component={Details}
+          options={{ header: () => <Header /> }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+export { Navigator };
