fd3d2ed08222872a895a9b612279e42abfd959eb
robuGdada <paudelronish@gmail.com>
2023-12-23 14:22:37 +0545

    ui fix

--- src/components/bottomTabBar.tsx
+++ src/components/bottomTabBar.tsx
@@ -6,61 +6,63 @@ import { SafeAreaView } from "react-native-safe-area-context";
 export function MyTabBar({ state, descriptors, navigation }: any) {
   return (
     <View style={{ flexDirection: "row" }}>
-      {state.routes.map(
-        (
-          route: { key: string | number; name: any; params: any },
-          index: any
-        ) => {
-          console.log(index);
-          const { options } = descriptors[route.key];
-          const label =
-            options.tabBarLabel !== undefined
-              ? options.tabBarLabel
-              : options.title !== undefined
-              ? options.title
-              : route.name;
+      {state.routes.map((route, index) => {
+        console.log(index);
+        const { options } = descriptors[route.key];
+        const label =
+          options.tabBarLabel !== undefined
+            ? options.tabBarLabel
+            : options.title !== undefined
+            ? options.title
+            : route.name;
 
-          const isFocused = state.index === index;
+        const isFocused = state.index === index;
 
-          const onPress = () => {
-            const event = navigation.emit({
-              type: "tabPress",
-              target: route.key,
-              canPreventDefault: true,
-            });
+        const onPress = () => {
+          const event = navigation.emit({
+            type: "tabPress",
+            target: route.key,
+            canPreventDefault: true,
+          });
 
-            if (!isFocused && !event.defaultPrevented) {
-              navigation.navigate(route.name, route.params);
-            }
-          };
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name, route.params);
+          }
+        };
 
-          const onLongPress = () => {
-            navigation.emit({
-              type: "tabLongPress",
-              target: route.key,
-            });
-          };
+        const onLongPress = () => {
+          navigation.emit({
+            type: "tabLongPress",
+            target: route.key,
+          });
+        };
 
-          return (
-            <TouchableOpacity
-              accessibilityRole="button"
-              activeOpacity={1}
-              accessibilityState={isFocused ? { selected: true } : {}}
-              accessibilityLabel={options.tabBarAccessibilityLabel}
-              testID={options.tabBarTestID}
-              onPress={onPress}
-              onLongPress={onLongPress}
-              style={{ flex: 1, backgroundColor: "black" }}
-            >
-              <SafeAreaView edges={["bottom"]}>
-                <Text style={{ color: isFocused ? "yellow" : "white" }}>
-                  {label}
-                </Text>
-              </SafeAreaView>
-            </TouchableOpacity>
-          );
-        }
-      )}
+        return (
+          <TouchableOpacity
+            accessibilityRole="button"
+            activeOpacity={1}
+            accessibilityState={isFocused ? { selected: true } : {}}
+            accessibilityLabel={options.tabBarAccessibilityLabel}
+            testID={options.tabBarTestID}
+            onPress={onPress}
+            onLongPress={onLongPress}
+            style={{
+              flex: 1,
+              backgroundColor: "#0F0F0F",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <SafeAreaView edges={["bottom"]}>
+              <Text
+                style={{ color: isFocused ? "yellow" : "white", padding: 15 }}
+              >
+                {label}
+              </Text>
+            </SafeAreaView>
+          </TouchableOpacity>
+        );
+      })}
     </View>
   );
 }
